bf09f5aacdb829b6d9c164cf7dd630b53ae03b6c
Adib Rahim <adibrahim27@gmail.com>
2021-03-13 11:37:57 -0500

    ar

--- help.js
+++ help.js
@@ -6,45 +6,28 @@ function validate(){
     var message = document.getElementById("message").value;
     var error_message = document.getElementById("error_message");
     
-    error_message.style.padding = "10px";}
-    
-    
     var text;
     if(name.length < 10){
-      text = "Please Enter valid Name";
-      error_message.innerHTML = text;
+      alert ("Please enter a valid Full Name");
+     
       return false;
     }
     if(subject.length < 10){
-      text = "Please Enter Correct Subject";
-      error_message.innerHTML = text;
+      alert ("Please enter a Correct Subject containing at LEAST 10 characters");
       return false;
     }
-    if(isNaN(phone) || phone.length != 6){
-      text = "Please Enter valid Phone Number";
-      error_message.innerHTML = text;
-      return false;
-    }
-    if(email.indexOf("@") == -1 || email.length < 10){
-      text = "Please Enter valid Email";
-      error_message.innerHTML = text;
+    if(isNaN(phone) || phone.length != 9){
+      alert ("Please enter a valid Phone Number containing 9 digits");
       return false;
     }
     if(message.length <= 100){
-      text = "Please Enter More Than 100 Characters";
-      error_message.innerHTML = text;
+      alert("Please enter more than 100 characters for the message.");
       return false;
     }
-
-    if(validationfails){
-
-      return false;
-    }
-    else{
-
-    document.form.submit();
+    if(document.form.submit()) {
     alert("Form Submitted Successfully!");
     return true;
     }
+    return false;
   }
 
