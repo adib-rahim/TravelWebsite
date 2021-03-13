ad6b167ab221d080c105f3b761d65aab6ce2f42e
Jorge <57515225+jlcalleu18@users.noreply.github.com>
2021-03-12 22:30:32 -0500

    jorge

--- app.py
+++ app.py
@@ -1,41 +1,41 @@
 from flask import Flask, render_template, request
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:0350@localhost/bmi'
-db = SQLAlchemy(app)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:0350@localhost/bmi'
+# db = SQLAlchemy(app)
 
 
-class Data(db.Model):
-    __tablename__ = "data"
-    id = db.Column(db.Integer, primary_key=True)
-    email_ = db.Column(db.String(120), unique=True)
-    height = db.Column(db.Integer)
-    weight = db.Column(db.Integer)
+# class Data(db.Model):
+#     __tablename__ = "data"
+#     id = db.Column(db.Integer, primary_key=True)
+#     email_ = db.Column(db.String(120), unique=True)
+#     height = db.Column(db.Integer)
+#     weight = db.Column(db.Integer)
 
-    def __init__(self, email_, height_, weight_):
-        self.email_ = email_
-        self.height = height_
-        self.weight = weight_
+#     def __init__(self, email_, height_, weight_):
+#         self.email_ = email_
+#         self.height = height_
+#         self.weight = weight_
 
 
 @app.route("/")
+def help():
+    return render_template("help.html")
+
+
+@app.route("/index", methods=['POST'])
 def index():
-    return render_template("index.html")
-
-
-@app.route("/thankyou", methods=['POST'])
-def thankyou():
     if request.method == 'POST':
         email = request.form["email_name"]
-        height = request.form["height_name"]
-        weight = request.form["weight_name"]
-        print(request.form)
-        data = Data(email, height, weight)
-        db.session.add(data)
-        db.session.commit()
-        return render_template("thankyou.html")
+        # height = request.form["height_name"]
+        # weight = request.form["weight_name"]
+        # print(request.form)
+        # data = Data(email, height, weight)
+        # db.session.add(data)
+        # db.session.commit()
+        return render_template("index.html")
 
 
 if __name__ == '__main__':
